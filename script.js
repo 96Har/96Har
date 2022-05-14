c5e5c6fe1e20b24b757da97b14a8ffe903602166
96Har <105552159+96Har@users.noreply.github.com>
2022-05-14 13:52:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+
+$('#btn').click(function(){
+	$('.big-banner').click(function(){
+		$(this).css('background-image', 'url(https://media.istockphoto.com/photos/red-background-striped-illustration-picture-id1152326544?k=20&m=1152326544&s=612x612&w=0&h=kHM9qxqIJ8nV8NXhf8iyTAQet5GoN6kEp7cBq0ZHOBg=)');
+	})
+});
